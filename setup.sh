c8afb8f3c664d61ebe8b3f4e45798f49b94de2eb
MadhuAtBerkeley <madhu.hegde@gmail.com>
2021-10-10 18:09:48 -0700

    setup env

---
+++ setup.sh
@@ -0,0 +1,9 @@
+conda create -n w251_project python=3.9 jupyter
+conda activate w251_project
+conda install dlib
+conda install -c akode face_recognition_models
+conda install -c conda-forge face_recognition
+conda install certifi click dotmap tqdm Pillow numpy imutils requests
+conda install -c pytorch pytorch torchvision
+
+
