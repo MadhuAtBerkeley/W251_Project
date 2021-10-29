60a55f7ec2b7b84e0ea134f534a70512fb6be61e
MadhuAtBerkeley <mrhegde@ischool.berkeley.edu>
2021-10-28 20:48:15 -0700

    add new files

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from .models.inception_resnet_v1 import InceptionResnetV1
 from .models.mtcnn import MTCNN, PNet, RNet, ONet, prewhiten, fixed_image_standardization
 from .models.utils.detect_face import extract_face
 from .models.utils import training
+from .utils.box_utils import nms, _preprocess, calibrate_box, get_image_boxes, convert_to_square
+from .utils.first_stage import run_first_stage
 
 import warnings
 warnings.filterwarnings(
---
+++ setup.py
@@ -0,0 +1,4 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(name='W251Package', version='1.0.0', packages=find_packages())
